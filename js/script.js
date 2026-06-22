b6742e830675db7355e0f6719cea54c742d36290
KevinFernandes <kevinleven392@gmail.com>
2026-06-22 08:51:41 -0300

    other acessibility fixes and good pratices fixes

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ const messageA = document.getElementById("messageA");
 const consentBox = document.getElementById("consentBox");
 
 //All error "p"
-const pErrorL = document.querySelectorAll("p");
+const pErrorL = form.querySelectorAll("p");
 
 const fnError = document.getElementById("fnError");
 const snError = document.getElementById("snError");
@@ -26,7 +26,7 @@ const boxError = document.getElementById("boxError");
 
 
 function isEmpty(value){
-    return value == ""
+    return value.trim() === ""
 }
 
  function isValid(value){
@@ -39,26 +39,33 @@ function isEmpty(value){
 const submitB = document.querySelector("button");
 
 
-submitB.addEventListener("click", submit);
+form.addEventListener("submit", submit);
 
-function submit(){
+function submit(event){
 
+    event.preventDefault()
 
     let valid = true
 
     messageSent.classList.remove("displayF")
     inpList.forEach(inp =>{
         inp.classList.remove("inpError")
+        inp.removeAttribute("aria-invalid");
     })
     pErrorL.forEach(p => {
     p.classList.remove("pError");
     p.textContent = ""
-});
+    });
+    meError.classList.remove("pError");
+    messageA.classList.remove("inpError");
+    messageA.removeAttribute("aria-invalid");
+
 
     if(isEmpty(inpFN.value)){
         fnError.classList.add("pError")
         inpFN.classList.add("inpError")
         fnError.textContent = "This field is required"
+        inpFN.setAttribute("aria-invalid", "true");
         valid = false
     }
 
@@ -66,6 +73,7 @@ function submit(){
         snError.classList.add("pError")
         inpSN.classList.add("inpError")
         snError.textContent = "This field is required"
+        inpSN.setAttribute("aria-invalid", "true");
         valid = false
     }
 
@@ -73,16 +81,20 @@ function submit(){
         emError.classList.add("pError")
         inpEmail.classList.add("inpError")
         emError.textContent = "This field is required"
+        inpEmail.setAttribute("aria-invalid", "true");
         valid = false
     }else if(!isValid(inpEmail.value)){
         emError.classList.add("pError")
         emError.textContent = "Please enter a valid email address"
+        inpEmail.setAttribute("aria-invalid", "true");
         valid = false
     }
 
         if(!(generalE.checked || supportR.checked)){
         radError.classList.add("pError")
         radError.textContent = "Please select a query type"
+        generalE.setAttribute("aria-invalid", "true");
+        supportR.setAttribute("aria-invalid", "true");
         valid = false
     }
 
@@ -90,12 +102,14 @@ function submit(){
         meError.classList.add("pError")
         messageA.classList.add("inpError")
         meError.textContent = "This field is required"
+        messageA.setAttribute("aria-invalid", "true");
         valid = false
     }
     
         if(!consentBox.checked){
         boxError.classList.add("pError")
         boxError.textContent = "To submit this form, please consent to being contacted"
+        consentBox.setAttribute("aria-invalid", "true");
         valid = false
         }
 
